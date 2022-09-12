ba7ea6698ef1a44622d859a43bc5feef94f64bc3
Maxim <smarog@yandex.ru>
2022-09-12 12:08:40 +0300

    Итоговое задание Lesson 1 : Calculator

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int result = 1;
+        char sign = '^';
+
+        if (b != 0) {
+            if (sign == '+') {
+                result = a + b;
+            } else if (sign == '-') {
+                result = a - b;
+            } else if (sign == '*') {
+                result = a * b; 
+            } else if (sign == '/') {
+                result = a / b; 
+            } else if (sign == '^') {
+                for (int i = 1; i <= b; i++) {
+                    result *= a;
+                }
+            } else if (sign == '%') {
+                result = a % b;
+            }
+        }
+
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}
