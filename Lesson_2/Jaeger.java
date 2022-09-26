4e6f9e5161045d11550041d48819e8475617ce5c
Maxim <smarog@yandex.ru>
2022-09-26 12:56:18 +0300

    Второй пункт итогового ДЗ урока 2

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,92 @@
+public class Jaeger {
+    
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int strength;
+    private int armor;
+
+    public void setModelname(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    boolean drift() {
+        return true;
+    }
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    String scanKaiju() {
+        return "scanning";
+    }
+
+    void useVortexCannon() {
+        System.out.println("activated");
+    }
+
+    public Jaeger() {
+        modelName = "Cherno Alpha";
+        mark = "Mark-1";
+        origin = "Russia";
+        height = 85.34f;
+        weight = 2.412f;
+        strength = 10;
+        armor = 10;
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,31 @@
+public class JaegerTest {
+    
+    public static void main(String[] args) {
+        Jaeger strikerEureka = new Jaeger();
+        Jaeger chernoAlpha = new Jaeger();
+
+        strikerEureka.setModelname("Striker Eureka");
+        strikerEureka.setMark("Mark-5");
+        strikerEureka.setOrigin("Australia");
+        strikerEureka.setHeight(76.2f);
+        strikerEureka.setWeight(1.85f);
+        strikerEureka.setArmor(9);
+
+        System.out.println("Обнаружены роботы :");
+        System.out.printf("Modelname ->\t%s\t| %s%n",
+                strikerEureka.getModelName(), chernoAlpha.getModelName());
+        System.out.printf("Mark -> \t%s\t\t| %s%n",
+                strikerEureka.getMark(), chernoAlpha.getMark());
+        System.out.printf("Origin ->\t%s \t| %s%n",
+                strikerEureka.getOrigin(), chernoAlpha.getOrigin());
+        System.out.printf("Height ->\t%.2f \t\t| %.2f%n",
+                strikerEureka.getHeight(), chernoAlpha.getHeight());
+        System.out.printf("Weight ->\t%.2f \t\t| %.2f%n",
+                strikerEureka.getWeight(), chernoAlpha.getWeight());
+        System.out.printf("Strength ->\t%d \t\t| %d%n",
+                strikerEureka.getStrength(), chernoAlpha.getStrength());
+        System.out.printf("Armor ->\t%d \t\t| %d%n",
+                strikerEureka.getArmor(), chernoAlpha.getArmor());
+        
+    }
+}
