3900bda85c56226cd977f5a50b1ae233c32272a9
Maxim <smarog@yandex.ru>
2023-09-29 02:00:56 +0300

    CalculatorTest - класс 1) Введена переменная expression 2) введенное мат. выражение передается в метод calculate(), а не в setMathExpression(). Метод setMathExpression() удален 3) вывод результата вынесен в отдельный метод printResultMessage 4) по окончанию каждого вычисления выводится сообщение "Хотите продолжить вычисления? [yes/no]"
    Calculator - класс
    1) Если знак не поддерживается, нужно выводить в default - именно там же произошла эта ошибка - выполнено
    2) в switch не нужно делать явное приведение типа к double - это выполнится автоматически. Явно привел только операцию деления.
    3) toString - убрал

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,35 +1,25 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    private int a;
-    private int b;
-    private char sign;
-
-    public char getSign() {
-        return sign;
-    }
-    public void setMathExpression(String expression) {
+    public double calculate(String expression) {
         String[] mathExpression = expression.split(" ");
 
-        a = Integer.parseInt(mathExpression[0]);
-        sign = mathExpression[1].charAt(0);
-        b = Integer.parseInt(mathExpression[2]);
-    }
+        int a = Integer.parseInt(mathExpression[0]);
+        char sign = mathExpression[1].charAt(0);
+        int b = Integer.parseInt(mathExpression[2]);
 
-    public double calculate() {
-        return switch (sign) {
-            case '+' -> (double) a + b;
-            case '-' -> (double) a - b;
-            case '*' -> (double) a * b;
-            case '/' -> (double) a / b;
-            case '^' -> Math.pow(a, b);
-            case '%' -> (double) a % b;
-            default -> Double.MIN_VALUE;
-        };
-    }
+        double result = Double.MIN_VALUE;
 
-    @Override
-    public String toString() {
-        return a + " " + sign + " " + b;
+        switch (sign) {
+            case '+' -> result = a + b;
+            case '-' -> result = a - b;
+            case '*' -> result = a * b;
+            case '/' -> result = (double) a / b;
+            case '^' -> result = Math.pow(a, b);
+            case '%' -> result = a % b;
+            default -> System.out.println("Ошибка: знак " + sign + " не поддерживается");
+        }
+
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,30 +4,33 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        String answer;
-
         Calculator calc = new Calculator();
         Scanner input = new Scanner(System.in);
         input.useDelimiter("\n");
 
+        String answer;
+
         do {
             System.out.print("Введите математическое выражение : ");
-            answer = input.next();
+            String expression = input.next();
 
-            calc.setMathExpression(answer);
+            double result = calc.calculate(expression);
 
-            double result = calc.calculate();
+            printResultMessage(expression, result);
+            
+            do {
+                System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
+                answer = input.next();
+            } while (!(answer.equals("yes") || answer.equals("no")));
 
-            if (result == Double.MIN_VALUE) {
-                System.out.println("Ошибка: знак " + calc.getSign() + " не поддерживается");
-            } else if (result % 1 == 0) {
-                System.out.print(calc + " = " + (int) result);
-            } else {
-                System.out.format("%s = %.3f \n", calc, result);
-            }
-
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            answer = input.next();
         } while (!answer.equals("no"));
+    }
+
+    private static void printResultMessage(String expression, double result) {
+        if (result % 1 == 0) {
+            System.out.print(expression + " = " + (int) result);
+        } else {
+            System.out.format("%s = %.3f \n", expression, result);
+        }
     }
 }
