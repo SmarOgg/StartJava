890935bfd6b3dde5196b0ee65fba3312ca790b94
Maxim <smarog@yandex.ru>
2023-10-02 01:28:32 +0300

    Calculator, CalculatorTest 1) перепишите вывод результата через тернарный оператор
    2) сделайте метод calculate() — статическим. При этом создавать экземпляр Calculator уже не нужно
    
    3)используйте новый switch expression (1, 2)
    
    4) проверяйте:
        что вводимые числа — целые и положительные
         длину вводимого мат. выражения
    
    5) для каждой ошибки выкидывайте исключение с подходящим сообщением
    программа не должна падать (закрываться), если введены не удовлетворяющие условиям данные (используйте try-catch). В качестве класса исключений используйте RuntimeException
    
    6) при выкидывании любого исключения результат вычислений выводиться не должен

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,24 +1,41 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    public double calculate(String expression) {
+    public static double calculate(String expression) throws CalculatorException {
         String[] mathExpression = expression.split(" ");
+        
+        if (mathExpression.length < 3) {
+            throw new CalculatorException("В выражении должны быть два числа, знак операции и разделены пробелами");
+        }
 
-        int a = Integer.parseInt(mathExpression[0]);
-        char sign = mathExpression[1].charAt(0);
-        int b = Integer.parseInt(mathExpression[2]);
+        try {
+            int a = Integer.parseInt(mathExpression[0]);
+            char sign = mathExpression[1].charAt(0);
+            int b = Integer.parseInt(mathExpression[2]);
 
-        return switch (sign) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> a * b;
-            case '/' -> (double) a / b;
-            case '^' -> Math.pow(a, b);
-            case '%' -> a % b;
-            default -> {
-                System.out.println("Ошибка: знак " + sign + " гн поддерживается");
-                yield Double.MIN_VALUE;
-            }
-        };
+            if (a < 0 || b < 0) throw new CalculatorException("Числа должны быть положительными");
+
+            return switch (sign) {
+                case '+' -> a + b;
+                case '-' -> a - b;
+                case '*' -> a * b;
+                case '/' -> (double) a / b;
+                case '^' -> Math.pow(a, b);
+                case '%' -> a % b;
+                default -> {
+                    System.out.println("Ошибка: знак " + sign + " не поддерживается");
+                    yield Double.MIN_VALUE;
+                }
+            };
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return Double.MIN_VALUE;
+    }
+
+    static class CalculatorException extends Exception {
+        public CalculatorException(String message) {
+            super(message);
+        }
     }
 }
